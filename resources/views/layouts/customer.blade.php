3c84c62b3432feb853f3cb85d0cc3c087c24fca0
Md. Jayed Hasan <60888930+JayedHasan232@users.noreply.github.com>
2023-03-20 15:58:07 +0600

    favicon added

--- resources/views/layouts/customer.blade.php
+++ resources/views/layouts/customer.blade.php
@@ -7,6 +7,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'TribuAssist') }}</title>
+    <link rel="shortcut icon" href="{{asset('logo.png')}}" type="image/x-icon">
 
     <!-- Font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,6 +6,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="shortcut icon" href="{{asset('logo.png')}}" type="image/x-icon">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
